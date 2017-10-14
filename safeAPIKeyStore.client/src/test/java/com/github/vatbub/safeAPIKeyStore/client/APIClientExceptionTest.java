8733a01646b4293487e6f4c5b935a1bdc3b85428
vatbub <vatbub123+github@googlemail.com>
2017-10-15 01:56:37 +0200

    Fixed naming of a test class for maven to be able to find it

--- safeAPIKeyStore.client/src/test/java/com/github/vatbub/safeAPIKeyStore/client/APIClientExceptionTest.java
+++ safeAPIKeyStore.client/src/test/java/com/github/vatbub/safeAPIKeyStore/client/APIClientExceptionTest.java
@@ -1,5 +1,26 @@
 package com.github.vatbub.safeAPIKeyStore.client;
 
+/*-
+ * #%L
+ * safeAPIKeyStore.client
+ * %%
+ * Copyright (C) 2016 - 2017 Frederik Kammel
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
 import com.github.vatbub.common.core.Common;
 import com.github.vatbub.common.core.logging.FOKLogger;
 import org.junit.*;
@@ -11,7 +32,7 @@ import java.nio.file.Files;
 import java.util.Properties;
 import java.util.concurrent.TimeoutException;
 
-public class APIClientExceptionTests {
+public class APIClientExceptionTest {
     public static final String apiKey1Name = "apiKey1";
     public static final String apiKey1Value = "12345";
     public static final String apiKey2Name = "apiKey2";
